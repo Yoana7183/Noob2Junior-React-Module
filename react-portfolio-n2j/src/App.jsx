2339c9283e4b356443b7890182c958332d51f0d2
y nakova <yoana.nakova@yahoo.com>
2023-03-17 14:55:48 +0200

    Vite 3.0 setup / added tailwind css

--- react-portfolio-n2j/src/App.jsx
+++ react-portfolio-n2j/src/App.jsx
@@ -1,8 +1,9 @@
 import Welcome from "./components/Welcome"
+import "/src/css/main.css"
 
 export default function App() {
     return (
-        <>
+             <>
             <Welcome />
         </>
     )
