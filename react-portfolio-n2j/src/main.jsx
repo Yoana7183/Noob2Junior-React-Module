385d5e6c6ba8fab56e433f64fa97f47184bf0204
y nakova <yoana.nakova@yahoo.com>
2023-03-23 16:51:53 +0200

    added basic home page

--- react-portfolio-n2j/src/main.jsx
+++ react-portfolio-n2j/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import HomePage from './Landing-Page';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>
 );
