6e9b0b665b41d0843e826a97899d0e411806d7a1
y nakova <yoana.nakova@yahoo.com>
2023-03-23 14:50:40 +0200

    added extention eslint/prettier

--- react-portfolio-n2j/file1.js
+++ react-portfolio-n2j/file1.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-const title = 'React';
-
 function App() {
   return (
     <div>
--- react-portfolio-n2j/src/components/Welcome.jsx
+++ react-portfolio-n2j/src/components/Welcome.jsx
@@ -5,12 +5,11 @@ export default function Welcome() {
   return (
     <>
       <div className="wrapper">
-        <h1>'eeee '</h1>
+        <h1>eeee</h1>
 
         <h1 className="text-3xl font-bold underline text-center bg-green-900">
           Hello world!
         </h1>
-
         <img src={sam} alt="Sammy Image" width={200} height={200} />
       </div>
     </>
