1a673410a85356ff85f1c812d82280171c3532a0
y nakova <yoana.nakova@yahoo.com>
2023-03-28 10:03:49 +0300

    added trackable current page

--- react-portfolio-n2j/src/Navbar.jsx
+++ react-portfolio-n2j/src/Navbar.jsx
@@ -1,13 +1,37 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 import Button from './components/Button';
-
 const Navbar = () => {
+  let currentProject = useLocation();
+
+  switch (currentProject.pathname) {
+    case '/loopstudio':
+      currentProject = 'Landing Page Loopstudios';
+      break;
+    case '/equalize':
+      currentProject = 'Landing Page Equalizer';
+      break;
+    case '/git':
+      currentProject = 'GitHub user search app';
+      break;
+    case '/tipscalc':
+      currentProject = 'Tips calculator App';
+      break;
+    case '/e-commerce':
+      currentProject = 'E-commerce product page';
+      break;
+    case '/':
+      currentProject = 'Home page';
+      break;
+  }
+
   return (
     <>
       <nav>
         <ul>
           <li>Home</li>
-          <li>Current Project: </li>
+          <li>
+            Current Project: <div>{currentProject}</div>
+          </li>
           <div className="bg-indigo-500">
             <Link to="/">
               <Button title={'BACK'} />
