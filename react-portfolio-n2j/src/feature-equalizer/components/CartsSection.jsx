adcf4f2b6c4debd3fffed326ba16d50a0d6f6a95
y nakova <yoana.nakova@yahoo.com>
2023-04-04 17:09:03 +0300

    fully responsive xl/lg/md

--- react-portfolio-n2j/src/feature-equalizer/components/CartsSection.jsx
+++ react-portfolio-n2j/src/feature-equalizer/components/CartsSection.jsx
@@ -1,8 +1,8 @@
 const CartSection = () => {
   return (
-    <div className="mt-[15rem] mr-[-5%] ml-[-5%] lg:mr-16 lg:ml-0 lg:mt-[18rem] md:mr-6 md:ml-1 md:mt-[15rem] sm:ml-[-3rem] flex ">
+    <div className="mt-[15rem] mr-[-5%] ml-[-5%] lg:mr-16 lg:ml-0 lg:mt-[18rem] md:mr-6 md:ml-1 md:mt-[15rem] sm:ml-[-3rem] flex md:flex  ">
       <div className="bg-black xl:h-[600px] xl:w-[1110px] lg:w-[850px] lg:h-[500px] flex rounded-xl md:w-[696px] md:h-[600px] ">
-        <div className="w-[100%]  xl:z-40 lg:z-40 md:z-40 ">
+        <div className="w-[100%]  md:z-40 ">
           <img
             className=" bg-cover xl:h-[642px] xl:w-[312px] xl:ml-[4rem] xl:mt-[-180px] md:w-[270px] md:h-[556px] md:mt-[-120px] md:ml-[3rem]"
             src="/src/feature-equalizer/assets/illustration-app.png"
@@ -10,18 +10,17 @@ const CartSection = () => {
             srcSet=""
           />
         </div>
-        <div className="lg:w-[350px] lg:z-0 md:z-0 md:w-[280px] md:m-[0px]">
+        <div className="lg:w-[350px] md:w-[280px]">
           <img
-            className="  lg:bg-cover xl:ml-[-35rem] lg:ml-[-22rem] md:ml-[-10rem]  "
+            className="  lg:bg-cover xl:ml-[-35rem] lg:ml-[-25rem] md:ml-[-16rem]  "
             src="/src/feature-equalizer/assets/bg-pattern-2.svg"
             alt=""
             srcSet=""
           />
         </div>
-        <div></div>
       </div>
-      <div className="bg-[#FA7453] roundedn-xl text-white xl:w-[446px] xl:h-[625px] xl:mt-[7rem] xl:ml-[-40rem] z-20 pb-[5rem] xl:p-10 lg:ml-[-28rem] lg:w-[400px] lg:h-[600px] lg:mt-[7rem] lg:rounded-xl md:w-[399px] md:h-[200px] md:m-[0px] md:z-1 md:mr-[10rem] ">
-        <div className="xl:text-4xl xl:font-bold font-titleEqualizer xl:mb-[1.5rem]">
+      <div className="bg-[#FA7453] roundedn-xl text-white xl:w-[446px] xl:h-[600px] xl:mt-[7rem] xl:ml-[-40rem] pb-[5rem] xl:p-10 lg:ml-[-28rem] lg:w-[400px] lg:h-[580px] lg:mt-[7rem] lg:rounded-xl md:w-[399px] md:h-[580px] md:ml-[-29rem] md:z-40 md:mt-[10rem] md:p-10 md:rounded-xl ">
+        <div className="xl:text-4xl xl:font-bold font-titleEqualizer xl:mb-[1.5rem] md:text-3xl md:font-bold md:mb-[1rem]">
           Premium EQ
         </div>
         <div className="leading-9 font-Equalizer mb-[1.5rem]">
@@ -36,13 +35,13 @@ const CartSection = () => {
         <div className="flex flex-col">
           <button
             type="button"
-            className="cursor-pointer bg-black xl:w-[333px] xl:h-[61px] rounded-xl mb-4"
+            className="cursor-pointer bg-black xl:w-[333px] h-[61px] rounded-xl mb-4 "
           >
             iOS Download iOS Download
           </button>
           <button
             type="button"
-            className="cursor-pointer bg-white text-black xl:w-[333px] xl:h-[61px] rounded-xl"
+            className="cursor-pointer bg-white text-black xl:w-[333px] h-[61px] rounded-xl"
           >
             Android Download
           </button>
--- react-portfolio-n2j/src/feature-equalizer/components/Footer.jsx
+++ react-portfolio-n2j/src/feature-equalizer/components/Footer.jsx
@@ -3,20 +3,20 @@ const Footer = () => {
     <div className=" xl:w-[88%] lg:mb-[5rem] lg:w-[80%] h-[30px] mt-[10rem] flex  lg:flex-row lg: md:flex-col sm:flex-col lg:justify-between">
       <div>
         <img
-          className="lg:pt-13 "
+          className="lg:pt-13 mb-[2rem]"
           src="/src/feature-equalizer/assets/logo.svg"
           alt=""
           srcSet=""
         />
       </div>
-      <div>
+      <div className="">
         <div>All rights reserved © Equalizer 2021</div>
         <div>Have any problems? Contact us via social media or</div>
-        <div>
+        <div className="md:mb-[3rem]">
           email us at <strong>equalizer@example.com</strong>
         </div>
       </div>
-      <div className="flex">
+      <div className="flex md:justify-end md:mt-[-6rem]">
         <img
           className="p-1"
           src="/src/feature-equalizer/assets/icon-facebook.svg"
