0d0c4a41a4a9010624fa72f0f1ebec194e7e3116
y nakova <yoana.nakova@yahoo.com>
2023-04-04 17:36:37 +0300

    temporary commit/ starting working on mobile view

--- react-portfolio-n2j/src/feature-equalizer/components/CartsSection.jsx
+++ react-portfolio-n2j/src/feature-equalizer/components/CartsSection.jsx
@@ -1,25 +1,25 @@
 const CartSection = () => {
   return (
     <div className="mt-[15rem] mr-[-5%] ml-[-5%] lg:mr-16 lg:ml-0 lg:mt-[18rem] md:mr-6 md:ml-1 md:mt-[15rem] sm:ml-[-3rem] flex md:flex  ">
-      <div className="bg-black xl:h-[600px] xl:w-[1110px] lg:w-[850px] lg:h-[500px] flex rounded-xl md:w-[696px] md:h-[600px] ">
-        <div className="w-[100%]  md:z-40 ">
+      <div className="bg-black xl:h-[600px] xl:w-[1110px] lg:w-[850px] lg:h-[500px] flex rounded-xl md:w-[696px] md:h-[600px] sm:w-[600px] sm:h-[520px] ">
+        <div className="w-[100%]  md:z-40 sm:z-40 ">
           <img
-            className=" bg-cover xl:h-[642px] xl:w-[312px] xl:ml-[4rem] xl:mt-[-180px] md:w-[270px] md:h-[556px] md:mt-[-120px] md:ml-[3rem]"
+            className=" bg-cover xl:h-[642px] xl:w-[312px] xl:ml-[4rem] xl:mt-[-180px] md:w-[270px] md:h-[556px] md:mt-[-120px] md:ml-[3rem] sm:h-[500px] sm:w-[220px] sm:mt-[-6rem] sm:ml-6"
             src="/src/feature-equalizer/assets/illustration-app.png"
             alt=""
             srcSet=""
           />
         </div>
-        <div className="lg:w-[350px] md:w-[280px]">
+        <div className="lg:w-[350px] md:w-[280px] sm:w-[220px]">
           <img
-            className="  lg:bg-cover xl:ml-[-35rem] lg:ml-[-25rem] md:ml-[-16rem]  "
+            className="  lg:bg-cover xl:ml-[-35rem] lg:ml-[-25rem] md:ml-[-16rem] sm:ml-[-15rem]  "
             src="/src/feature-equalizer/assets/bg-pattern-2.svg"
             alt=""
             srcSet=""
           />
         </div>
       </div>
-      <div className="bg-[#FA7453] roundedn-xl text-white xl:w-[446px] xl:h-[600px] xl:mt-[7rem] xl:ml-[-40rem] pb-[5rem] xl:p-10 lg:ml-[-28rem] lg:w-[400px] lg:h-[580px] lg:mt-[7rem] lg:rounded-xl md:w-[399px] md:h-[580px] md:ml-[-29rem] md:z-40 md:mt-[10rem] md:p-10 md:rounded-xl ">
+      <div className="bg-[#FA7453] roundedn-xl text-white xl:w-[446px] xl:h-[600px] xl:mt-[7rem] xl:ml-[-40rem] pb-[5rem] xl:p-10 lg:ml-[-28rem] lg:w-[400px] lg:h-[580px] lg:mt-[7rem] lg:rounded-xl md:w-[399px] md:h-[580px] md:ml-[-29rem] md:z-40 md:mt-[10rem] md:p-10 md:rounded-xl sm:w-[330px] sm:h-[520px] sm:rounded-xl sm:p-8 sm:mt-[7rem] sm:ml-[-25rem] sm:z-40 ">
         <div className="xl:text-4xl xl:font-bold font-titleEqualizer xl:mb-[1.5rem] md:text-3xl md:font-bold md:mb-[1rem]">
           Premium EQ
         </div>
--- react-portfolio-n2j/src/feature-equalizer/components/Footer.jsx
+++ react-portfolio-n2j/src/feature-equalizer/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <div className=" xl:w-[88%] lg:mb-[5rem] lg:w-[80%] h-[30px] mt-[10rem] flex  lg:flex-row lg: md:flex-col sm:flex-col lg:justify-between">
+    <div className=" xl:w-[88%] md:w-[90%] lg:mb-[5rem] lg:w-[80%] h-[30px] mt-[10rem] flex  lg:flex-row lg: md:flex-col sm:flex-col lg:justify-between sm:mb-[12rem]">
       <div>
         <img
-          className="lg:pt-13 md:mb-[2rem]"
+          className="lg:pt-13 md:mb-[2rem] sm:mb-[1rem]"
           src="/src/feature-equalizer/assets/logo.svg"
           alt=""
           srcSet=""
@@ -12,11 +12,11 @@ const Footer = () => {
       <div className="">
         <div>All rights reserved © Equalizer 2021</div>
         <div>Have any problems? Contact us via social media or</div>
-        <div className="md:mb-[3rem]">
+        <div className="md:mb-[3rem] sm:mb-[1rem]">
           email us at <strong>equalizer@example.com</strong>
         </div>
       </div>
-      <div className="flex md:justify-end md:mt-[-6rem] xl:mt-0 lg:mt-0">
+      <div className="flex md:justify-end md:mt-[-6rem] xl:mt-0 lg:mt-0  ">
         <img
           className="p-1"
           src="/src/feature-equalizer/assets/icon-facebook.svg"
