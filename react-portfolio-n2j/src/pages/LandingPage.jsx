9ce57ba9ab5386c1fc517abb73d9464b46448469
y nakova <yoana.nakova@yahoo.com>
2023-04-13 13:45:13 +0300

    added new feature

--- react-portfolio-n2j/src/pages/LandingPage.jsx
+++ react-portfolio-n2j/src/pages/LandingPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import BackButton from '../components/back-button-navbar';
+import Loopstudios from '../feature-loopstudios/Loopstudios';
+
 export default function LandingPage() {
   return (
     <div>
       <BackButton />
-      <div className="bg-indigo-400 bg-cover">
-        <h1>Langing Page</h1>
-      </div>
+      <Loopstudios />
     </div>
   );
 }
