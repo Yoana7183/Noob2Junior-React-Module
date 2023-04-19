b9ec2f61db6eaacc9d68072b3b70703e71fd68fe
y nakova <yoana.nakova@yahoo.com>
2023-04-19 17:27:52 +0300

    card position text fixed

--- react-portfolio-n2j/src/feature-loopstudios/components/Cards.jsx
+++ react-portfolio-n2j/src/feature-loopstudios/components/Cards.jsx
@@ -3,35 +3,51 @@ const Cards = () => {
     <div className="mt-36 ml-32 mr-32">
       <div>
         <div className="flex justify-between mb-10">
-          <div className=" justify-start">OUR CREATIONS</div>
-          <div className=" justify-end">
-            <button>SEE ALL</button>
+          <div className="font-sans lg:text-5xl lg:font-thin">
+            OUR CREATIONS
+          </div>
+          <div className="">
+            <button className="lg:mr-2 lg:w-[160px] lg:h-[50px] bg-black text-white">
+              SEE ALL
+            </button>
           </div>
         </div>
-        <div className="grid lg:grid-cols-4 gap-10 lg:w-[1200px]">
-          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-deep-earth.jpg')] lg:w-[256px]  lg:h-[450px] bg-no-repeat ">
-            DEEP EARTH
+        <div className="grid lg:grid-cols-4 gap-10 lg:w-[100%]">
+          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-deep-earth.jpg')] lg:w-[256px]  lg:h-[450px] bg-no-repeat lg:text-3xl text-white lg:relative ">
+            <p className="lg:absolute lg:inset-x-0 lg:bottom-0  ">DEEP EARTH</p>
           </div>
-          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-night-arcade.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat">
-            NIGHT ARCAD
+          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-night-arcade.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat  lg:text-3xl text-white lg:relative">
+            <p className="lg:absolute lg:inset-x-0 lg:bottom-0  ">
+              NIGHT ARCAD
+            </p>
           </div>
-          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-soccer-team.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat">
-            SOCCER TEAM VR
+          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-soccer-team.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat  lg:text-3xl text-white lg:relative">
+            <p className="lg:absolute lg:inset-x-0 lg:bottom-0  ">
+              SOCCER TEAM VR
+            </p>
           </div>
-          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-grid.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat">
-            THE GRID
+          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-grid.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat  lg:text-3xl text-white lg:relative">
+            <p className="lg:absolute lg:inset-x-0 lg:bottom-0  ">THE GRID</p>
           </div>
-          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-from-above.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat">
-            FROM UP ABOVE VR
+          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-from-above.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat  lg:text-3xl text-white lg:relative">
+            <p className="lg:absolute lg:inset-x-0 lg:bottom-0  ">
+              FROM UP ABOVE VR
+            </p>
           </div>
-          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-pocket-borealis.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat">
-            POCKET BOREALIS
+          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-pocket-borealis.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat  lg:text-3xl text-white lg:relative">
+            <p className="lg:absolute lg:inset-x-0 lg:bottom-0  ">
+              POCKET BOREALIS
+            </p>
           </div>
-          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-curiosity.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat">
-            THE CURIOSITY
+          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-curiosity.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat  lg:text-3xl text-white lg:relative">
+            <p className="lg:absolute lg:inset-x-0 lg:bottom-0  ">
+              POCKET BOREALIS
+            </p>
           </div>
-          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-fisheye.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat">
-            MAKE IT FISHEYE
+          <div className="lg:bg-[url('/src/feature-loopstudios/images/desktop/image-fisheye.jpg')] lg:w-[256px] lg:h-[450px] bg-no-repeat  lg:text-3xl text-white lg:relative">
+            <p className="lg:absolute lg:inset-x-0 lg:bottom-0  ">
+              MAKE IT FISHEYE
+            </p>
           </div>
         </div>
       </div>
