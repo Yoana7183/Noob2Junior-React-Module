fdf734aabfb03f1ef13beab39d812c0ba46b7754
y nakova <yoana.nakova@yahoo.com>
2023-04-19 17:46:58 +0300

    added footer with details

--- react-portfolio-n2j/src/feature-loopstudios/components/Footer.jsx
+++ react-portfolio-n2j/src/feature-loopstudios/components/Footer.jsx
@@ -1,14 +1,40 @@
+import Navbar from './Navbar';
 const Footer = () => {
   return (
     <div className="lg:h-[160px] w-full bg-black mt-40 text-white ">
-      <div className="flex">
-        <div>
-          <div>logo</div>
-          <div>menu</div>
+      <div className=" flex pl-32 justify-between  ">
+        <div className="mt-5 mb-5">
+          <div className="font-alata text-white w-[190px] text-4xl ">
+            loopstudios
+          </div>
+          <div className="pl-5 mt-5">
+            <Navbar />
+          </div>
         </div>
-        <div>
-          <div>icons</div>
-          <div>rights</div>
+        <div className="pr-32">
+          <div className="flex mt-6 mb-6 ">
+            <img
+              src="src\feature-loopstudios\images\icon-facebook.svg"
+              alt=""
+              srcSet=""
+            />
+            <img
+              src="src\feature-loopstudios\images\icon-twitter.svg"
+              alt=""
+              srcSet=""
+            />
+            <img
+              src="src\feature-loopstudios\images\icon-pinterest.svg"
+              alt=""
+              srcSet=""
+            />
+            <img
+              src="src\feature-loopstudios\images\icon-instagram.svg"
+              alt=""
+              srcSet=""
+            />
+          </div>
+          <div>© 2021 Loopstudios. All rights reserved.</div>
         </div>
       </div>
     </div>
