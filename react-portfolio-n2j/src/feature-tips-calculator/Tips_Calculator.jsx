7fc3aa798707e8b229b370e0e2785b1e2587d905
y nakova <yoana.nakova@yahoo.com>
2023-04-26 15:51:34 +0300

    added basic structure

---
+++ react-portfolio-n2j/src/feature-tips-calculator/Tips_Calculator.jsx
@@ -0,0 +1,19 @@
+const TipsCalculator = () => {
+  return (
+    <div className="bg-teal-50 w-[100%] h-[70rem] m-0">
+      <div className="flex flex-cols-1 justify-center pt-16">
+        <div>
+          <div>SPLI</div>
+          <div>TTER</div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="flex rounded-md w-[920px] h-[481px] bg-white mt-36">
+          <div>sss</div>
+          <div>bbbb</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default TipsCalculator;
--- react-portfolio-n2j/src/pages/TipsCalculator.jsx
+++ react-portfolio-n2j/src/pages/TipsCalculator.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import BackButton from '../components/back-button-navbar';
+import Tips_Calculator from '../feature-tips-calculator/Tips_Calculator';
 export default function TipsCalculator() {
   return (
     <div>
       <BackButton />
-      <div>
-        <h1>TipsCalculator</h1>
-      </div>
+      <Tips_Calculator />
     </div>
   );
 }
