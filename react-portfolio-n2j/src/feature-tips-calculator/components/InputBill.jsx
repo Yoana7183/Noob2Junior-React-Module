b69fd24a46fa743767dcbdfb38d44954f9aeef92
y nakova <yoana.nakova@yahoo.com>
2023-05-09 15:53:49 +0300

    added validation in input

--- react-portfolio-n2j/src/feature-tips-calculator/components/InputBill.jsx
+++ react-portfolio-n2j/src/feature-tips-calculator/components/InputBill.jsx
@@ -1,14 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { TipsContext as BillConext } from '../Tips_Calculator';
 
 const InputBill = () => {
   const { inputsInObject, setinputsInObject } = useContext(BillConext);
+  const [error, setError] = useState('');
 
   const handleClick = (event) => {
-    const amountBill = event.target.value;
-
+    const amountBill = event.target.value.trim();
     setinputsInObject((prev) => ({ ...prev, bill: amountBill }));
+    console.log(amountBill);
+    if (isNaN(amountBill)) {
+      setError('Моля, въведете число.');
+    } else if (amountBill < 0) {
+      setError('Моля, въведете положително число.');
+    } else {
+      setError('');
+    }
   };
 
   if (inputsInObject.bill === 0) {
@@ -16,8 +24,9 @@ const InputBill = () => {
   }
   return (
     <div>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
       <input
-        type="number"
+        type="text"
         id="bill"
         name="bill"
         value={inputsInObject.bill}
