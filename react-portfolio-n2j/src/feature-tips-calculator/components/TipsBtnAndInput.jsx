0f49ad10ad399e5881a07c8cd87cee418d87d0b6
y nakova <yoana.nakova@yahoo.com>
2023-05-09 14:27:37 +0300

    added dinamyc btn / set new tip percent

--- react-portfolio-n2j/src/feature-tips-calculator/components/TipsBtnAndInput.jsx
+++ react-portfolio-n2j/src/feature-tips-calculator/components/TipsBtnAndInput.jsx
@@ -1,34 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { TipsContext as PercentTipContext } from '../Tips_Calculator';
 
 const TipsBtnAndInput = () => {
   const { inputsInObject, setinputsInObject } = useContext(PercentTipContext);
+  const [percent, setPercent] = useState(0);
 
-  const handleClick = (event) => {
-    const percentTip = event.target.value;
+  useEffect(() => {
+    setinputsInObject((prev) => ({ ...prev, tip: percent }));
+  }, [percent]);
 
-    setinputsInObject((prev) => ({ ...prev, tip: percentTip }));
+  const handleChange = (event) => {
+    let inputPercvet = event.target.value;
+
+    setinputsInObject((prev) => ({ ...prev, tip: inputPercvet }));
   };
   if (inputsInObject.tip === 0) {
     inputsInObject.tip = '';
   }
-
+  const handleButtonClick = (newPercent) => {
+    setPercent(newPercent);
+  };
   return (
     <div>
-      <button id="5" onChange={handleClick}>
+      <button id="5" onClick={() => handleButtonClick(5)}>
         5%
       </button>
-      <button id="10">10%</button>
-      <button id="15">15%</button>
-      <button id="25">25%</button>
-      <button id="50"> 50%</button>
+      <button id="10" onClick={() => handleButtonClick(10)}>
+        10%
+      </button>
+      <button id="15" onClick={() => handleButtonClick(15)}>
+        15%
+      </button>
+      <button id="25" onClick={() => handleButtonClick(25)}>
+        25%
+      </button>
+      <button id="50" onClick={() => handleButtonClick(50)}>
+        50%
+      </button>
       <input
         placeholder="Custom"
         type="number"
         id="tip"
         name="tip"
         value={inputsInObject.tip}
-        onChange={handleClick}
+        onChange={handleChange}
       />
     </div>
   );
