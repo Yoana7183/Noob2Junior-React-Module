fe02f132ca4e1b16c8355b7bcf908bfba4c6e5e0
y nakova <yoana.nakova@yahoo.com>
2023-06-20 13:24:56 +0300

    fixed hover on icons on modal menu

--- react-portfolio-n2j/src/feature-eCommerce/components/ModalGallery.jsx
+++ react-portfolio-n2j/src/feature-eCommerce/components/ModalGallery.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ProductGallery from './images';
-
 const ModalGallery = (props) => {
   return (
     <div className=" fixed top-0 left-0 w-full h-full  items-center justify-center hidden sm:flex ">
@@ -10,24 +9,15 @@ const ModalGallery = (props) => {
           <div className="relative">
             <div className="flex items-center">
               <div
-                className="rounded-full bg-white w-[56px] h-[56px] flex justify-center cursor-pointer absolute inset-y-15 left-[-30px]"
+                className="rounded-full bg-white w-[56px] h-[56px] flex justify-center cursor-pointer absolute inset-y-15 left-[-30px]  hover:bg-center bg-[url('/src/feature-eCommerce/images/icon-previous.svg')] bg-no-repeat bg-center hover:bg-[url('/src/feature-eCommerce/images/icons8-previous-26.png')]"
                 onClick={props.prevImg}
-              >
-                <img
-                  className="w-[10px] h-[16px] mt-5"
-                  src="src\feature-eCommerce\images\icon-previous.svg"
-                  alt=""
-                  srcSet=""
-                ></img>
-              </div>
+              ></div>
               <div className="md:w-[550px] sm:w-[450px]">
-                <div className="flex justify-end mb-3">
-                  <img
-                    className="w-8 cursor-pointer  text-orange-400"
-                    src="src/feature-eCommerce/images/icon-close.png"
-                    alt=""
-                    onClick={() => props.setIsModal(false)}
-                  />
+                <div
+                  className="flex justify-end mb-1 "
+                  onClick={() => props.setIsModal(false)}
+                >
+                  <div className="w-[34px] h-[36px] mb-3 bg-no-repeat bg-cover bg-[url('/src/feature-eCommerce/images/icon-close.svg')] hover:bg-[url('/src/feature-eCommerce/images/icon-close_hover.png')] "></div>
                 </div>
                 <img
                   className="md:w-[550px] sm:w-[450px] rounded-xl"
@@ -37,16 +27,9 @@ const ModalGallery = (props) => {
               </div>
 
               <div
-                className="rounded-full bg-white w-[56px] h-[56px] flex justify-center cursor-pointer absolute inset-y-15 right-[-30px] "
+                className="rounded-full bg-white w-[56px] h-[56px] flex justify-center cursor-pointer absolute inset-y-15 right-[-30px] bg-[url('/src/feature-eCommerce/images/icon-next.svg')] bg-no-repeat bg-center hover:bg-[url('/src/feature-eCommerce/images/icons8-next-26.png')] "
                 onClick={props.nextImg}
-              >
-                <img
-                  className="w-[10px] h-[16px] mt-5  "
-                  src="src\feature-eCommerce\images\icon-next.svg"
-                  alt=""
-                  srcSet=""
-                ></img>
-              </div>
+              ></div>
             </div>
             <div className="sm:flex mt-4 px-4 lg:ml-[5%] sm:ml-[4rem] md:ml-10 hidden">
               {props.thumbNail}
