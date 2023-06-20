a8c219bd94f499d04eb747f4e08ea36bafcae242
y nakova <yoana.nakova@yahoo.com>
2023-06-20 16:01:15 +0300

    partly refactored

--- react-portfolio-n2j/src/feature-equalizer/Equalizer.jsx
+++ react-portfolio-n2j/src/feature-equalizer/Equalizer.jsx
@@ -5,25 +5,9 @@ const Equalizer = () => {
   return (
     <div className="w-full h-full">
       <div
-        className="lg:bg-[url('/src/feature-equalizer/assets/bg-main-desktop.png')]
-       md:bg-[url('/src/feature-equalizer/assets/bg-main-tablet.png')] 
-       sm:bg-[url('/src/feature-equalizer/assets/bg-main-tablet.png')] 
-       bg-[url('/src/feature-equalizer/assets/bg-main-mobile.png')] 
-        w-full
-         xl:h-[120rem]
-          lg:h-[115rem]
-           md:h-[110rem]
-           bg-no-repeat 
-           bg cover
-            pl-5
-             lg:pl-40
-              md:pl-10 
-              md:pr-30 
-              sm:pl-20
-              sm:bg-cover
-              sm:h-[100rem] 
-               sm:pr-3 
-               pr-5 "
+        className="lg:bg-[url('/src/feature-equalizer/assets/bg-main-desktop.png')] md:bg-[url('/src/feature-equalizer/assets/bg-main-tablet.png')] 
+       sm:bg-[url('/src/feature-equalizer/assets/bg-main-tablet.png')] bg-[url('/src/feature-equalizer/assets/bg-main-mobile.png')] 
+        w-full xl:h-[120rem] lg:h-[115rem]  md:h-[110rem] bg-no-repeat bg cover pl-5  lg:pl-40 md:pl-10 md:pr-30 sm:pl-20 sm:bg-cover sm:h-[100rem] sm:pr-3  pr-5 "
       >
         <Header />
         <CartSection />
--- react-portfolio-n2j/src/feature-equalizer/components/Footer.jsx
+++ react-portfolio-n2j/src/feature-equalizer/components/Footer.jsx
@@ -1,34 +1,12 @@
 const Footer = () => {
   return (
     <div
-      className=" ml-[calc(50%-11.7rem)]
-     p-5 
-     h-[30px]
-     mt-[5rem]
-      flex 
-     xl:w-[88%]
-     xl:ml-[0]
-      lg:ml-[0] 
-      lg:justify-between 
-      lg:mb-[5rem]
-      lg:flex-row
-       lg:w-[80%] 
-      md:ml-[0]
-        flex-col
-         mb-[15rem] 
-          md:w-[90%] 
-              md:flex-col 
-              sm:flex-col 
-              sm:mb-[12rem] 
-              sm:p-0
-              sm:ml-[0]"
+      className="ml-[calc(50%-11.7rem)] p-5 h-[30px] mt-[5rem] flex xl:w-[88%] xl:ml-[0] lg:ml-[0] lg:justify-between 
+    lg:mb-[5rem] lg:flex-row lg:w-[80%] md:ml-[0] flex-col mb-[15rem] md:w-[90%] md:flex-col sm:flex-col sm:mb-[12rem] sm:p-0 sm:ml-[0]"
     >
       <div>
         <img
-          className=" mb-[1rem]
-           lg:pt-13
-            md:mb-[2rem]
-             sm:mb-[1rem]"
+          className=" mb-[1rem] lg:pt-13 md:mb-[2rem] sm:mb-[1rem]"
           src="/src/feature-equalizer/assets/logo.svg"
           alt=""
           srcSet=""
@@ -37,21 +15,11 @@ const Footer = () => {
       <div className="">
         <div>All rights reserved © Equalizer 2021</div>
         <div>Have any problems? Contact us via social media or</div>
-        <div
-          className=" mb-[2rem] 
-        md:mb-[3rem]
-         sm:mb-[1rem]"
-        >
+        <div className=" mb-[2rem] md:mb-[3rem] sm:mb-[1rem]">
           email us at <strong>equalizer@example.com</strong>
         </div>
       </div>
-      <div
-        className="flex
-      xl:mt-0 
-      lg:mt-0 
-       md:justify-end
-        md:mt-[-6rem] "
-      >
+      <div className="flex xl:mt-0 lg:mt-0 md:justify-end md:mt-[-6rem] ">
         <div className="flex items-center mr-4 ">
           <div className="bg-[url('/src/feature-equalizer/assets/icon-facebook.svg')] w-[25px] h-[25px] bg-cover hover:bg-[url('/src/feature-equalizer/assets/facebook-hover.svg')]  hover:w-[25px] hover:h-[25px]"></div>
         </div>
