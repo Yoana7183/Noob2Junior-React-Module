e0b25d50e0d843ee0565b5891a98485bf9701202
y nakova <yoana.nakova@yahoo.com>
2023-07-04 17:25:46 +0300

    fixed/error page

--- react-portfolio-n2j/src/App.jsx
+++ react-portfolio-n2j/src/App.jsx
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
@@ -10,13 +9,6 @@ import LandingPage from './pages/LandingPage';
 import LandingPageEz from './pages/LandingPageEz';
 import TipsCalculator from './pages/TipsCalculator';
 
-const NotFound = () => {
-  return (
-    <div>
-      <h1>Page Not Found</h1>
-    </div>
-  );
-};
 function App() {
   return (
     <BrowserRouter>
@@ -42,11 +34,25 @@ function App() {
             <Route path="loopstudio" element={<LandingPage />} />
             <Route path="equalize" element={<LandingPageEz />} />
             <Route path="tipscalc" element={<TipsCalculator />} />
-            <Route path="/*" element={<NotFound />} />
+            {/* Catch-all route for handling unknown paths */}
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </ErrorBoundary>
     </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-red-500 to-pink-500 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold mb-4 text-center text-red-500">
+          Oops!
+        </h1>
+        <p className="text-lg text-gray-800 text-center">Page not found.</p>
+      </div>
+    </div>
   );
 }
 
