19352ec1c800554744a12515d416b22cb341359e
y nakova <yoana.nakova@yahoo.com>
2023-07-19 17:03:30 +0300

    separated the theme from the user information object in the context

--- react-portfolio-n2j/src/feature-gitHub/GitHubFindDev.jsx
+++ react-portfolio-n2j/src/feature-gitHub/GitHubFindDev.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import SearchBarAndSubmitButton from './components/InputSeach';
 
 import PersonalUserInformation from './components/PersonalUserInformation';
@@ -7,25 +7,18 @@ import LinksAndLocation from './components/Links';
 import { DataContext } from './context/DataContext';
 
 const GitHubFindDev = () => {
-  const { userData, loadFetchedUser } = useContext(DataContext);
-  const [toggledTheme, setToggledTheme] = useState(userData.theme);
+  const { userData, theme, setTheme } = useContext(DataContext);
 
   const toggleTheme = () => {
-    setToggledTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
-  useEffect(() => {
-    loadFetchedUser(() => ({
-      ...userData,
-      theme: toggledTheme,
-    }));
-  }, [toggledTheme]);
 
   const toggleDarkToLightStyleBackground =
-    userData.theme === 'dark'
+    theme === 'dark'
       ? 'bg-gitDarkSpaceBackground'
       : 'bg-gitLightSpaceBackground';
   const toggleDarkToLightStyleContainers =
-    userData.theme === 'dark'
+    theme === 'dark'
       ? ' bg-gitContainerBlackBackground text-white'
       : 'bg-white text-gitTextOnLight';
 
@@ -41,24 +34,13 @@ const GitHubFindDev = () => {
             <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
           </div>
         ) : null}
-        {userData.expiredAPIkey ? (
-          <div className={toggleDarkToLightStyleBackground}>
-            Ooops..A problem has occurred, most likely due to a problem with API
-            key!
-          </div>
-        ) : null}
-        {userData.serviceUnavailable ? (
-          <div className={toggleDarkToLightStyleBackground}>
-            Ooops..503 Service Unavailable error status code try the request
-            again later!
-          </div>
-        ) : null}
+
         <div
           className={`flex justify-between pb-6 ${toggleDarkToLightStyleBackground}`}
         >
           <header
             className={`text-3xl font-black ${
-              userData.theme === 'dark'
+              theme === 'dark'
                 ? ' bg-gitDarkSpaceBackground text-white'
                 : 'bg-gitLightSpaceBackground text-black'
             }`}
@@ -69,7 +51,7 @@ const GitHubFindDev = () => {
             className={`flex justify-between  ${toggleDarkToLightStyleBackground}`}
             onClick={toggleTheme}
           >
-            {userData.theme == 'light' ? (
+            {theme == 'light' ? (
               <div className="hover:text-black">
                 <div className="mt-[1rem] flex justify-center">
                   Dark
