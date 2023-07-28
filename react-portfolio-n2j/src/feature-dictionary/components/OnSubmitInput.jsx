7f8421b1f2f802ad8c10c8f6efe2799a76de6fcd
Yoana Nakova <yoana.nakova@YAHOO.COM>
2023-07-28 10:21:46 +0300

    added basic components and context logic

---
+++ react-portfolio-n2j/src/feature-dictionary/components/OnSubmitInput.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OnSubmitInput = () => {
+  return <div></div>;
+};
+
+export default OnSubmitInput;
---
+++ react-portfolio-n2j/src/feature-dictionary/components/ResultContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ResultContainer = () => {
+  return <div></div>;
+};
+
+export default ResultContainer;
