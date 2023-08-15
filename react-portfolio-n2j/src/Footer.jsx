1b0b895ff7f266160da7790ec9731516c694562c
Yoana Nakova <yoana.nakova@YAHOO.COM>
2023-08-15 14:51:32 +0300

    fixed main footer  / added some padding

--- react-portfolio-n2j/src/Footer.jsx
+++ react-portfolio-n2j/src/Footer.jsx
@@ -1,8 +1,11 @@
 const Footer = () => {
   return (
     <div className="text-[10px] h-16 text-gray-700 flex justify-center">
-      Challenge by{'  Noob2Junior  '}
-      Frontend Mentor . Coded by <a href="#"> {'  Nakova  Yoana  '} </a>.
+      <p>Challenge by Noob2Junior Frontend Mentor . Coded by</p>
+      <a href="#">
+        <p className="pl-1">Nakova Yoana</p>
+      </a>
+      .
     </div>
   );
 };
