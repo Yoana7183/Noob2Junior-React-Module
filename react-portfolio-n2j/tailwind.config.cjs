33f28eb95144f2852cdc2a5c3ce0031d91a13973
Yoana Nakova <yoana.nakova@YAHOO.COM>
2023-08-21 19:16:16 +0300

    temporary

--- react-portfolio-n2j/tailwind.config.cjs
+++ react-portfolio-n2j/tailwind.config.cjs
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: ['/index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {
       fontFamily: {
