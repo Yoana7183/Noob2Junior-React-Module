9e47960cc5eba00ef2764c172925760d006582a9
Yoana Nakova <yoana.nakova@YAHOO.COM>
2023-08-21 15:18:56 +0300

    added responsiv header

--- react-portfolio-n2j/src/CV_Component/components/Header.jsx
+++ react-portfolio-n2j/src/CV_Component/components/Header.jsx
@@ -3,23 +3,27 @@ import PersonalDetails from './PersonalDetails';
 
 const Header = () => {
   return (
-    <div className="p-5 font-serif md:mt-[4rem]">
-      <div className="flex md:justify-between px-16 ml-[3rem] mb-[2rem]">
-        <div className="mt-10 ">
-          <div>
-            <h1 className="lg:text-4xl tracking-tight md:mb-6">Yoana Nakova</h1>
-            <h2 className="lg:text-2xl text-sky-500 tracking-tight md:ml-[1.2rem]">
+    <div className="p-5 font-serif md:mt-[4rem] flex justify-center flex-col">
+      <div className="flex md:justify-between  flex-col sm:flex-row sm:px-10 lg:px-36 md:px-16 sm:ml-[3rem] md:mb-[2rem] md:border-none md:shadow-none border-b border-slate-500 font-bold mx-2 shadow-sm sm:pb-[4rem] md:pb-[2rem] px-0 pb-[3rem] sm:mt-10 ">
+        <div className="mt-10 flex justify-center sm:block">
+          <div className="sm:mr-5 mr-0">
+            <h1 className="sm:text-4xl text-3xl tracking-tight md:mb-6 pb-5 sm:pb-5 md:pb-2">
+              Yoana Nakova
+            </h1>
+            <h2 className="sm:text-2xl text-xl text-sky-500 tracking-tight md:ml-[1.2rem] ml-4 pb-5">
               React JS Developer
             </h2>
           </div>
         </div>
-        <div className="w-[10rem] md:mr-[3rem] mr-0 flex  md:block   ">
-          <img
-            src="/src/CV_Component/img/myImg.jpg"
-            alt=""
-            srcSet=""
-            className="w-[10rem] border-2 rounded-3xl border-gray-300 transition duration-300 hover:border-sky-500  hover:shadow-2xl"
-          />
+        <div className=" flex items-center justify-center md:block">
+          <div className="">
+            <img
+              src="/src/CV_Component/img/myImg.jpg"
+              alt=""
+              srcSet=""
+              className="w-[10rem] border-2 rounded-3xl border-gray-300 transition duration-300 hover:border-sky-500 hover:shadow-2xl"
+            />
+          </div>
         </div>
       </div>
       <PersonalDetails />
--- react-portfolio-n2j/src/CV_Component/components/PersonalDetails.jsx
+++ react-portfolio-n2j/src/CV_Component/components/PersonalDetails.jsx
@@ -1,47 +1,49 @@
 import React from 'react';
 
 const PersonalDetails = () => {
+  const divStyle = `flex flex-row mx-2 pb-2 sm:pb-0`;
+  const textStyle = ` text-slate-500 text-xs sm:text-base pl-2 lg:pl-0`;
   return (
     <>
-      <section className="flex flex-row mt-5 px-16">
-        <div className="flex flex-row mx-2">
+      <section className="flex lg:flex-row flex-col mt-5 px-5 sm:px-10 md:px-16">
+        <div className={divStyle}>
           <img
             src="src/CV_Component/img/phone.png"
             alt="Phone Icon"
             className="w-5 h-5 "
           />
-          <a className=" text-slate-500" href="tel:+359 886793109">
+          <a className={textStyle} href="tel:+359 886793109">
             +359 886793109
           </a>
         </div>
 
-        <div className="flex flex-row mx-2">
+        <div className={divStyle}>
           <img
             src="src/CV_Component/img/mail.png"
             alt="Phone Icon"
             className="w-5 h-5 "
           />
-          <a className=" text-slate-500" href="mailto: yoana.nakova@yahoo.com">
+          <a className={textStyle} href="mailto: yoana.nakova@yahoo.com">
             yoana.nakova@yahoo.com
           </a>
         </div>
-        <div className="flex flex-row mx-2">
+        <div className={divStyle}>
           <img
             src="src/CV_Component/img/link.png"
             alt="Phone Icon"
             className="w-5 h-5 mt-[3px] "
           />
-          <a className=" text-slate-500" href="https://github.com/Yoana7183">
+          <a className={textStyle} href="https://github.com/Yoana7183">
             https://github.com/Yoana7183
           </a>
         </div>
-        <div className="flex flex-row mx-2">
+        <div className={divStyle}>
           <img
             src="src/CV_Component/img/location.png"
             alt="Phone Icon"
             className="w-5 h-5 mt-[3px] "
           />
-          <p className=" text-slate-500">Sofia, Bulgaria</p>
+          <p className={textStyle}>Sofia, Bulgaria</p>
         </div>
       </section>
     </>
