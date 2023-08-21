ecbad041bb387485238b3d3a5275cfdae3ef993a
Yoana Nakova <yoana.nakova@YAHOO.COM>
2023-08-21 19:13:28 +0300

    dont read the img

--- react-portfolio-n2j/tailwind.config.cjs
+++ react-portfolio-n2j/tailwind.config.cjs
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {
       fontFamily: {
