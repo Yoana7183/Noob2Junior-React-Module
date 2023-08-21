bbf66592d81b16954e4ad4230deb72ddf329012f
Yoana Nakova <yoana.nakova@YAHOO.COM>
2023-08-21 23:18:01 +0300

    fixed modal gallery url path to img

--- react-portfolio-n2j/src/feature-eCommerce/components/images.js
+++ react-portfolio-n2j/src/feature-eCommerce/components/images.js
@@ -1,19 +1,19 @@
 const ProductGallery = [
   {
-    origin: 'src/feature-eCommerce/images/image-product-1.jpg',
+    origin: '/assetsECommerce/image-product-1.jpg',
     thumbnail: 'src/feature-eCommerce/images/image-product-1-thumbnail.jpg',
   },
   {
-    origin: 'src/feature-eCommerce/images/image-product-2.jpg',
-    thumbnail: 'src/feature-eCommerce/images/image-product-2-thumbnail.jpg',
+    origin: '/assetsECommerce/image-product-2.jpg',
+    thumbnail: '/assetsECommerce/image-product-2-thumbnail.jpg',
   },
   {
-    origin: 'src/feature-eCommerce/images/image-product-3.jpg',
-    thumbnail: 'src/feature-eCommerce/images/image-product-3-thumbnail.jpg',
+    origin: '/assetsECommerce/image-product-3.jpg',
+    thumbnail: '/assetsECommerce/image-product-3-thumbnail.jpg',
   },
   {
-    origin: 'src/feature-eCommerce/images/image-product-4.jpg',
-    thumbnail: 'src/feature-eCommerce/images/image-product-4-thumbnail.jpg',
+    origin: '/assetsECommerce/image-product-4.jpg',
+    thumbnail: '/assetsECommerce/image-product-4-thumbnail.jpg',
   },
 ];
 
