da7e892560c3005b9b0856a981e9b97bb8cae182
Yoana Nakova <yoana.nakova@YAHOO.COM>
2023-08-21 23:13:56 +0300

    missed back button / changed url path

--- react-portfolio-n2j/src/components/back-button-navbar.jsx
+++ react-portfolio-n2j/src/components/back-button-navbar.jsx
@@ -5,7 +5,7 @@ const BackButton = () => {
     <div>
       <Link to="/">
         <div className="w-6 ml-4 mt-[-4rem] mb-5">
-          <img src="\src\img-icons\angles-left-solid.svg" alt="" srcSet="" />
+          <img src="\img-icons\angles-left-solid.svg" alt="" srcSet="" />
         </div>
       </Link>
     </div>
