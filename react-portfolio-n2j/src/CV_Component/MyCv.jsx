f324e846a9a913d2026125834113368c5e4e221e
Yoana Nakova <yoana.nakova@YAHOO.COM>
2023-08-21 17:35:42 +0300

    removed margin bottom

--- react-portfolio-n2j/src/CV_Component/MyCv.jsx
+++ react-portfolio-n2j/src/CV_Component/MyCv.jsx
@@ -7,12 +7,13 @@ import Education from './components/Education';
 import ProgrammingEducation from './components/ProgrammingEducation';
 import Languages from './components/Languages';
 import Certificates from './components/Certificates';
+
 const MyCv = () => {
   return (
-    <main className="flex justify-center  h-screen mt-[5rem] lg:mb-[120rem] md:mb-[175rem] sm:mb-[180rem] mb-[200rem] font-serif ">
+    <main className="flex justify-center  h-screen mt-[5rem]  font-serif ">
       <div className="flex justify-center border border-sky-500 rounded-2xl shadow-2xl flex-col h-max w-[90%] sm:w-[90%] lg:w-[890px] mb-16 p-0 md:pb-32 ">
         <Header />
-        <div className="flex md:px-16  px-6 mt-5  flex-col lg:flex-row">
+        <div className="flex md:px-16  px-6 mt-5  flex-col lg:flex-row mb-[80px]">
           <div className="lg:w-[60%] w-[100%]">
             <Summary />
             <Experience />
