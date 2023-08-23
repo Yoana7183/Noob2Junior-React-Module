30ec71a72d5731bae42654be861dd6b2b913e0b7
Yoana Nakova <yoana.nakova@YAHOO.COM>
2023-08-23 16:54:53 +0300

    fixed white bottom on specific vh

--- react-portfolio-n2j/src/feature-tips-calculator/Tips_Calculator.jsx
+++ react-portfolio-n2j/src/feature-tips-calculator/Tips_Calculator.jsx
@@ -21,9 +21,9 @@ const TipsCalculator = () => {
   }, [inputsInObject]);
 
   return (
-    <main ref={contentRef} className=" h-screen overflow-y-auto">
-      <div className="bg-teal-50 w-[100%] h-[100%]  lg:h-[70rem]  md:h-[100rem] m-0 font-mono">
-        <div className="flex flex-cols-1 pt-10 justify-center lg:pt-16 sm:pt-10">
+    <main ref={contentRef} className=" h-screen  bg-teal-50 overflow-y-auto">
+      <div className=" m-0 font-mono">
+        <div className="flex flex-cols-1 pt-10 justify-center  lg:pt-16 sm:pt-10">
           <header>
             <div className="text-4xl  text-buttonOfCalculatorAndRightSideBackground">
               SPLI
@@ -33,8 +33,8 @@ const TipsCalculator = () => {
             </div>
           </header>
         </div>
-        <div className="flex justify-center pt-10 ">
-          <div className="flex rounded-3xl shadow-2xl h-[60rem] sm:h-[75rem]  lg:w-[920px]  lg:h-[481px] sm:mt-16 bg-white lg:mt-36 lg:p-6 sm:p-6">
+        <div className="flex justify-center pt-10 mb-[25%] ">
+          <div className="flex rounded-3xl shadow-2xl h-[60rem] sm:h-[75rem]  lg:w-[920px] lg:h-[481px] sm:mt-16 bg-white lg:mt-36 lg:p-6 sm:p-6">
             <div className="grid grid-cols-1 lg:grid-cols-2">
               <div className=" ">
                 <LeftPart />
