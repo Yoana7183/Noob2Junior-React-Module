a1913eb8aefd54230c3485f3b0aa6590418e4117
Yoana Nakova <yoana.nakova@YAHOO.COM>
2023-09-07 18:45:38 +0300

    added responsive UI

--- react-portfolio-n2j/src/feature-tips-calculator/components/ModalHistoryResults.jsx
+++ react-portfolio-n2j/src/feature-tips-calculator/components/ModalHistoryResults.jsx
@@ -18,50 +18,63 @@ const ModalHistoryResults = ({
     getLocalStorageItems()
   );
 
-  if (localStorageData.length > 5) {
+  if (localStorageData.length > 4) {
     dequeueLocalStorageList();
-
     setLocalStorageData(getLocalStorageItems());
   }
 
   return (
     <section className="fixed top-0 left-0 w-full h-full items-center justify-center  flex">
       <div className="w-full h-full rounded-lg p-8 absolute top-0 left-0 bg-gray-800 bg-opacity-50">
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center mt-[10%] sm:mt-16">
           <div className="relative">
             <button
+              className="absolute top-2 right-2"
               onClick={() => {
                 setHistoryContainerStatus();
               }}
             >
-              close
+              <img
+                src="/assetsTipsCalc/close.svg"
+                alt="close"
+                className="w-7 h-7 cursor-pointer"
+              />
             </button>
             <div className="flex justify-center">
-              <div className="w-[250px] h-full bg-white">
+              <div className="w-[250px] sm:w-[350px] md:w-[400px] lg:w-[420px] h-full bg-white rounded-lg p-4">
+                <h2 className="text-lg font-semibold mb-4">History</h2>
                 {localStorageData.map((item) => {
                   return (
                     <div
                       key={uuidv4()}
-                      className="text-sm list-none border-b-2 border-buttonOfCalculatorAndRightSideBackground py-2 px-2"
+                      className="text-sm border-b-2 border-buttonOfCalculatorAndRightSideBackground py-2"
                     >
-                      <li>Bill: {item.bill}</li>
-                      <li>Percent Tip: {item.tip} %</li>
-                      <li>People: {item.people}</li>
-                      <li>
-                        Total: {item.total.toFixed(2)} / By Person:
+                      <p className="mb-1">Bill: ${item.bill}</p>
+                      <p className="mb-1">Percent Tip: {item.tip}%</p>
+                      <p className="mb-1">People: {item.people}</p>
+                      <p>
+                        Total: ${item.total.toFixed(2)} / By Person: $
                         {item.perPerson.toFixed(2)}
-                      </li>
+                      </p>
                     </div>
                   );
                 })}
-                <button
-                  onClick={() => {
-                    clearLocalStorage();
-                    setLocalStorageData(getLocalStorageItems());
-                  }}
-                >
-                  Delete history
-                </button>
+                {localStorageData.length > 0 && (
+                  <button
+                    className="flex items-center justify-center mt-4 text-buttonOfCalculatorAndRightSideBackground cursor-pointer"
+                    onClick={() => {
+                      clearLocalStorage();
+                      setLocalStorageData(getLocalStorageItems());
+                    }}
+                  >
+                    <img
+                      src="/assetsTipsCalc/delete.png"
+                      alt="delete"
+                      className="w-7 h-7 mr-2"
+                    />
+                    <p>Delete History</p>
+                  </button>
+                )}
               </div>
             </div>
           </div>
