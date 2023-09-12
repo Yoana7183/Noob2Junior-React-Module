e90f8b2bd0a2a7352e23d8bf66dfde96989e69c3
Yoana Nakova <yoana.nakova@YAHOO.COM>
2023-09-12 21:25:08 +0300

    fixed popup cart

--- react-portfolio-n2j/src/feature-eCommerce/components/PopUpCart.jsx
+++ react-portfolio-n2j/src/feature-eCommerce/components/PopUpCart.jsx
@@ -1,55 +1,71 @@
 import React, { useContext } from 'react';
 import { EcommerceContext } from '../context/EcommerceContext';
 import { NavLink } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 const Cart = () => {
-  const { setQuantity, cartStatus } = useContext(EcommerceContext);
-  const quantity = 10;
+  const { setCartStatus, removeItemFromCart, cartStatus, shoppingCartContent } =
+    useContext(EcommerceContext);
+
   return (
     <section
       className={`${
         cartStatus == 'open'
           ? 'opacity-100 relative z-20 flex justify-center'
           : 'opacity-0'
-      }`}
+      } `}
     >
-      <div className="w-[360px] h-[256px] border-2shadow-2xl rounded-xl bg-white	shadow-2xl xl:ml-[47rem] lg:ml-[40rem] md:ml-[25rem] sm:ml-[18rem] relative-">
+      <div className=" w-[360px] h-[256px] border-2shadow-2xl rounded-xl bg-white	shadow-2xl xl:ml-[47rem] lg:ml-[40rem] md:ml-[25rem] sm:ml-[18rem] relative-">
         <div className="">
           <div className="h-[67px] border-b-[1px] border-[#E4E9F2] pt-5 pl-3 ">
             Cart
           </div>
-          {quantity == 0 && (
-            <div className="w-[312px] h-[56px] text-gray-300 flex justify-center rounded-xl mt-16 ml-5">
-              <p>Your cart is empty.</p>
-            </div>
-          )}
-          {quantity > 0 && (
-            <div className="flex justify-around mt-7 ">
-              <figure className="w-[50px] h-[50px] ">
-                <img className="rounded" alt="Product_image" srcSet="" />
-              </figure>
-              <div>
-                <h2>Fall Limited Edition Sneakers</h2>
-                <div>
-                  <p>
-                    $125.00 x {quantity} ${quantity * 125.0}.00
-                  </p>
+          <div className="overflow-y-auto max-h-[150px]">
+            {shoppingCartContent.length == 0 && (
+              <div className="w-[312px] h-[56px] text-gray-300 flex justify-center rounded-xl mt-16 ml-5">
+                <p>Your cart is empty.</p>
+              </div>
+            )}
+            {shoppingCartContent.length > 0 &&
+              shoppingCartContent.map((item) => (
+                <div key={uuidv4()} className="flex justify-around mt-7 ">
+                  <figure className="w-[50px] h-[50px] ">
+                    <img
+                      className="rounded"
+                      alt="Product_image"
+                      src={item.IMG}
+                      srcSet=""
+                    />
+                  </figure>
+                  <div>
+                    <h2 className="w-[140px]">{item.TITLE}</h2>
+                    <div>
+                      <p>
+                        ${item.PRICE}x {item.DISCOUNT} ={item.TOTAL}
+                      </p>
+                    </div>
+                  </div>
+                  <figure
+                    className="pt-5 cursor-pointer"
+                    onClick={() => removeItemFromCart(item.ID, item.SIZE)}
+                  >
+                    <img src="\assetsECommerce\icon-delete.svg" alt="delete" />
+                  </figure>
                 </div>
-              </div>
-              <figure
-                className="pt-5 cursor-pointer"
-                onClick={() => setQuantity(0)}
-              >
-                <img src="\assetsECommerce\icon-delete.svg" alt="delete" />
-              </figure>
-            </div>
-          )}
-          {quantity > 0 && (
-            <NavLink to={`/e-commerce/shopping-cart`}>
-              <div className="w-[312px] h-[56px] bg-ecommerceOrangeColor hover:bg-hoverEcommerceOrangeColor   text-white flex justify-center rounded-xl mt-7 ml-5">
-                <button>Checkout</button>
-              </div>
-            </NavLink>
-          )}
+              ))}
+
+            {shoppingCartContent.length > 0 && (
+              <NavLink to={`/e-commerce/shopping-cart`}>
+                <div
+                  onClick={() => {
+                    setCartStatus('closed');
+                  }}
+                  className="w-[312px] h-[56px] bg-ecommerceOrangeColor hover:bg-hoverEcommerceOrangeColor   text-white flex justify-center rounded-xl mt-7 ml-5"
+                >
+                  <button>Checkout</button>
+                </div>
+              </NavLink>
+            )}
+          </div>
         </div>
       </div>
     </section>
